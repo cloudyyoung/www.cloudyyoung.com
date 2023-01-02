08fcac938efc171202583ae0993deee9a7e068f3
Yunfan Yang <cloudy.young@outlook.com>
2023-01-01 19:11:55 -0700

    Profile

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Home from './pages/Home/Home';
+import Profile from './pages/Profile/Profile';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route index element={<Home />} />
+        <Route path="/me" element={<Profile />} />
       </Routes>
     </Router>
   )
