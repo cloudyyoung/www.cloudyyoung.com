fd73589b596199176f1c70c9633219c696096431
Yunfan Yang <yunfan.yang@agvisorpro.com>
2023-07-16 20:30:29 -0600

    Something...?

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,60 @@
-import { useState } from 'react'
 import './App.css'
 
-import Grad_09 from "./assets/Grad_09.png"
-
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-      <div style={{background: "#fff"}}>
-        <img src={Grad_09} />
+      <div className='navbar'></div>
+      
+      <div className='hero-container'>
+        <div className='hero'>
+          <div className='blob-section'>
+            <div className='grad-9'></div>
+          </div>
+          <div className='info-section'>
+            <div>
+              <div className='name'>
+                Yunfan Yang
+              </div>
+              <div className='headline'>
+                <p>Software Development Intern, AGvisorPRO Inc.</p>
+                <p>BSc Computer Science, University of Calgary</p>
+                {/* <p>Calgary Alberta Canada</p> */}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className='greeting-container'>
+        <div className='greeting'>
+          <div className='greeting-short'>
+            Nice to meet you
+          </div>
+
+          <div className='greeting-headline'>
+            Hey! Welcome to my little space on the Internet. I'm Yunfan, I live and code.
+          </div>
+
+          <div className='bio'>
+            I focus on mainly web development.
+            As a computer science student, my disciplines drive me exploring all different areas in the fields of Computer Science, Data Science and Software Engineering. 
+          </div>
+
+          <div className='bio'>
+            I am always passionate about learning new technologies and building cool stuff.
+            You will be able to find some of my works here. They are rarely in the same areas as my interests are always changing from time to time. But I hope you will find something interesting.
+          </div>
+        </div>
+
+        <div className='greeting-blob'>
+          <div className='grad-18'></div>
+        </div>
+      </div>
+
+      <div className='footer'>
+        <div className='footer-content'>
+          <p>&copy; Yunfan Yang 2023</p>
+        </div>
       </div>
     </>
   )
