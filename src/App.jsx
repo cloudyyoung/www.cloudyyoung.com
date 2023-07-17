1a6553da17a261d514819e48342abfeb4d0b68c9
Yunfan Yang <yunfan.yang@agvisorpro.com>
2023-07-16 21:46:53 -0600

    Clean

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
-import { useState } from 'react'
 import './App.css'
 
-import Grad_09 from "./assets/Grad_09.png"
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div style={{background: "#fff"}}>
-        <img src={Grad_09} />
-      </div>
+      
     </>
   )
 }
