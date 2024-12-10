f38ab2e30699088211c4eebfd4e1f65059966127
Yunfan Yang <cloudy.young@outlook.com>
2024-12-10 03:02:09 -0700

    Minor

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ export interface NavbarProps {
 
 const Navbar = ({ title }: NavbarProps) => {
   return (
-    <div className='w-full flex justify-between uppercase z-10 sticky'>
+    <div className='w-full flex justify-between uppercase z-10 sticky top-0'>
       <div><Link to='/'>YY</Link></div>
       <div>{title}</div>
       <div><Link to='/info'>INFO</Link></div>
