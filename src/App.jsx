f9d9280c9d253becfbc1424855ded78093007d0d
Yunfan Yang <cloudy.young@outlook.com>
2024-12-09 19:10:48 -0700

    Update libraries

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,10 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom"
-import { ReactLenis } from '@studio-freight/react-lenis'
-import { useEffectOnce } from 'react-use'
-import { useRef } from 'react'
+import Lenis from 'lenis'
 import MouseFollower from "mouse-follower";
 import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
 import './App.css'
 import 'mouse-follower/dist/mouse-follower.min.css'
@@ -25,34 +24,23 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
-  const lenisRef = useRef()
-  useEffectOnce(() => {
-    function update(time) {
-      lenisRef.current?.lenis?.raf(time * 700)
-    }
-  
-    gsap.ticker.add(update)
-  
-    return () => gsap.ticker.remove(update)
-  })
+  new Lenis({
+    autoRaf: true,
+  });
 
-  useEffectOnce(() => {
+  useGSAP(() => {
     MouseFollower.registerGSAP(gsap);
-    const cursor = new MouseFollower({
+    new MouseFollower({
       visible: true,
       speed: 0.9,
       skewing: 2,
       container: document.body,
     });
-
-    return () => cursor.destroy();
-  })
+  });
 
   return (
     <>
-      <ReactLenis ref={lenisRef} autoRaf={false} root>
-        <RouterProvider router={router} />
-      </ReactLenis>
+      <RouterProvider router={router} />
     </>
   );
 }
